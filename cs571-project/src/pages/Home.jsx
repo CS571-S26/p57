7970bdc6e0961959ed0b9cdc2edc39a9aa2d8694
StephenSPS <stephen52419@gmail.com>
2026-03-17 22:10:44 -0500

    changes for submission

---
+++ cs571-project/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="container mt-4">
+      <h1>Welcome Home</h1>
+      <p>This is the home page.</p>
+      <button className="btn btn-primary mt-3">Click Me</button>
+    </div>
+  )
+}
+
+export default Home
